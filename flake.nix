9b28a1aab9fb449b54f0a3134c15bcf6997315df
Burak <burak.yn.dev@gmail.com>
2026-09-16 10:09:32 +0300

    fix: add flake and fix sdl submodule

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "chip-8 env";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages."${system}");
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            bear
+            gnumake
+          ];
+
+          env = {
+          };
+        };
+      });
+    };
+
+}
+
